452f2a4f446dbc92ce9e3dd56563bdd48585cc77
YUYEON91 <cybott2002@gmail.com>
2025-12-09 15:51:17 +0900

    Reapply "add 아이템 클래스"
    This reverts commit 69b4c20f1f38106d4701f2aa21dc990f69bb2ed8.

---
+++ Source/V12_the_game/Private/Items/V12ItemBase.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+// V12ItemBase.cpp
+
+#include "Items/V12ItemBase.h"
+
+// Sets default values
+AV12ItemBase::AV12ItemBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AV12ItemBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
