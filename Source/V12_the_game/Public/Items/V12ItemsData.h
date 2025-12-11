bbedc202009c9bd84bb8156d6ebc4dda0b3b847d
YUYEON91 <cybott2002@gmail.com>
2025-12-11 19:39:50 +0900

    Update 아이템 데이터 테이블 헤더파일 수정, 데이터 테이블 수정(uasset), Add 인벤토리 컴포넌트 추가

--- Source/V12_the_game/Public/Items/V12ItemsData.h
+++ Source/V12_the_game/Public/Items/V12ItemsData.h
@@ -4,8 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Engine/DataTable.h"
-#include "V12ItemBase.h"
 #include "V12ItemsData.generated.h"
+
+class AV12ItemBase;
 
 USTRUCT(BlueprintType)
 struct FV12ItemData : public FTableRowBase
@@ -13,17 +14,18 @@ struct FV12ItemData : public FTableRowBase
 	GENERATED_BODY()
 
 public:
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
-	TSubclassOf<AV12ItemBase> ItemClass;
+	FString ItemID;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
 	FString ItemName;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
-	float Duration; // 0이면 즉시 사용
+	int32 Weight = 1; // 확률 가중치
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
-	int32 Weight = 1; // 확률 가중치
+	TSubclassOf<AV12ItemBase> ItemClass;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item")
 	UTexture2D* ItemIcon;
